e27a31c0e11ce0f40e50f49e0653f00004721d4f
kasusoba <27213166+kasusoba@users.noreply.github.com>
2026-02-05 17:34:02 +0700

    init ye

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+export const userConfig = {
+	currently: "scrolling or smn",
+	socials: [
+		{
+			name: "github",
+			url: "https://github.com/faishalirwn",
+		},
+		{
+			name: "anilist",
+			url: "https://anilist.co/user/kasusoba/",
+		},
+		{
+			name: "last.fm",
+			url: "https://www.last.fm/user/kasusoba/",
+		},
+		{
+			name: "trakt",
+			url: "https://trakt.tv/users/kasusoba",
+		},
+	],
+};
